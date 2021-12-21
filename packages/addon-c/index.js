a6aee38be291c0ed9a5d71e263b6a8bc17abda78
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-12-21 14:28:22 -0500

    Reproduction success: has to do with typescript, somehow

--- packages/addon-c/index.js
+++ packages/addon-c/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const cacheKeyForTree = require('calculate-cache-key-for-tree');
+
 module.exports = {
   name: require('./package').name,
+
+  options: {
+    'ember-cli-babel': {
+      enableTypeScriptTransform: true,
+    },
+  },
+
+  // cacheKeyForTree(treeType) {
+  //   return cacheKeyForTree(treeType, this);
+  // },
 };
